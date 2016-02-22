7df08acb8363a0963f6a20276107c3e05152d16b
ajinkyakolhe112 <ajinkyakolhe112@gmail.com>
2016-02-22 18:52:58 +0530

    Pipe Fixing

---
+++ src/main/Pipes.java
@@ -0,0 +1,15 @@
+package main;
+
+/**
+ * Created by ajinkya on 22/2/16.
+ */
+public class Pipes {
+    public static int[] pipeFix(int[] numbers) {
+        int[] newNumbers = new int[numbers[numbers.length - 1] - numbers[0] + 1];
+        newNumbers[0] = numbers[0];
+        for (int i = 0; i < newNumbers.length; i++)
+            newNumbers[i] = newNumbers[0] + i;
+
+        return newNumbers;
+    }
+}
---
+++ src/test/PipesTest.java
@@ -0,0 +1,19 @@
+package test;
+
+/**
+ * Created by ajinkya on 22/2/16.
+ */
+import main.Pipes;
+import org.junit.Test;
+import static org.junit.Assert.assertArrayEquals;
+
+public class PipesTest {
+    @Test
+    public void tests() {
+        assertArrayEquals(new int[] {1,2,3,4,5,6,7,8,9}, Pipes.pipeFix(new int[] {1,2,3,5,6,8,9}));
+        assertArrayEquals(new int[] {1,2,3,4,5,6,7,8,9,10,11,12}, Pipes.pipeFix(new int[] {1,2,3,12}));
+        assertArrayEquals(new int[] {6,7,8,9}, Pipes.pipeFix(new int[] {6,9}));
+        assertArrayEquals(new int[] {-1,0,1,2,3,4}, Pipes.pipeFix(new int[] {-1,4}));
+        assertArrayEquals(new int[] {1,2,3}, Pipes.pipeFix(new int[] {1,2,3}));
+    }
+}
