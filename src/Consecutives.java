573b61a8ad4ca4577f90e65f31a8b0cf0fa0f252
ajinkya <ajinkyakolhe112@gmail.com>
2016-03-13 16:37:49 +0530

    Consecutives empty commit

---
+++ src/Consecutives.java
@@ -0,0 +1,11 @@
+import java.util.List;
+
+/**
+ * Created by ajinkya on 13/03/16.
+ */
+public class Consecutives {
+
+    public static List<Integer> sumConsecutives(List<Integer> s) {
+        // your code
+    }
+}
