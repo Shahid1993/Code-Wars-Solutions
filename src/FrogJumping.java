bd6d3536260406b567e2a4c4c06f6e57638c295d
ajinkya <ajinkyakolhe112@gmail.com>
2016-03-14 09:11:35 +0530

    FrogJumping, to avoid circular infinite loop. Keep list of visited nodes

--- src/FrogJumping.java
+++ src/FrogJumping.java
@@ -1,15 +1,21 @@
+import java.util.ArrayList;
+
 public class FrogJumping {
 
     public static int solution(int[] a) {
         int currentIndex = 0;
         int nextIndex = 0;
         int count = 0;
+        ArrayList<Integer> visitedNodes = new ArrayList<Integer>();
         while (true) {
             nextIndex = currentIndex + a[currentIndex];
             count++;
             if (nextIndex < 0 || nextIndex > a.length - 1)
                 return count;
+            if (visitedNodes.contains(nextIndex))
+                return -1;
             currentIndex = nextIndex;
+            visitedNodes.add(currentIndex);
         }
     }
 }
