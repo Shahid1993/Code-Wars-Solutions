c8881afe0770134fbb8d992e825d3964e7f7092e
ajinkya <ajinkyakolhe112@gmail.com>
2016-03-14 07:52:40 +0530

    Consecutives first commit

--- src/FrogJumping.java
+++ src/FrogJumping.java
@@ -1,6 +1,15 @@
 public class FrogJumping {
 
     public static int solution(int[] a) {
-        // Your code here
+        int i;
+        int count = 0;
+        for (i = 0; i < a.length; ) {
+            i = i + a[i];
+            count++;
+        }
+        if (i == a.length - 1)
+            return count + 1;
+        else
+            return -1;
     }
 }
