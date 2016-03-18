f98b24c29019bdc7826ce3507a03f1e131775b62
ajinkya <ajinkyakolhe112@gmail.com>
2016-03-18 14:16:24 +0530

    Pagination Code & Tested

--- src/PaginationHelper.java
+++ src/PaginationHelper.java
@@ -2,26 +2,30 @@ import java.util.List;
 
 public class PaginationHelper<I> {
 
+    List<I> collection;
+    int itemsPerPage;
+
     /**
      * The constructor takes in an array of items and a integer indicating how many
      * items fit within a single page
      */
     public PaginationHelper(List<I> collection, int itemsPerPage) {
-
+        this.collection = collection;
+        this.itemsPerPage = itemsPerPage;
     }
 
     /**
      * returns the number of items within the entire collection
      */
     public int itemCount() {
-
+        return collection.size();
     }
 
     /**
      * returns the number of pages
      */
     public int pageCount() {
-
+        return (int) Math.ceil((1.0 * collection.size()) / itemsPerPage);
     }
 
     /**
@@ -29,7 +33,14 @@ public class PaginationHelper<I> {
      * this method should return -1 for pageIndex values that are out of range
      */
     public int pageItemCount(int pageIndex) {
+        int pageCount = (int) Math.ceil((1.0 * collection.size()) / itemsPerPage);
+        if (pageIndex >= pageCount || pageIndex < 0)
+            return -1;
 
+        if (pageIndex == pageCount - 1 && collection.size() % itemsPerPage != 0)
+            return collection.size() % itemsPerPage;
+        else
+            return itemsPerPage;
     }
 
     /**
@@ -37,6 +48,9 @@ public class PaginationHelper<I> {
      * this method should return -1 for itemIndex values that are out of range
      */
     public int pageIndex(int itemIndex) {
-
+        if (itemIndex >= collection.size() || itemIndex < 0)
+            return -1;
+        else
+            return itemIndex / itemsPerPage;
     }
 }
--- test/PaginationHelperTest.java
+++ test/PaginationHelperTest.java
@@ -1,5 +1,27 @@
+import org.junit.Test;
+
+import java.util.Arrays;
+
+import static org.junit.Assert.assertEquals;
+
 /**
  * Created by ajinkya on 18/03/16.
  */
 public class PaginationHelperTest {
+    @Test
+    public void testOne() {
+        PaginationHelper<Character> helper = new PaginationHelper(Arrays.asList('a', 'b', 'c', 'd', 'e', 'f'), 4);
+        assertEquals(2, helper.pageCount()); //should == 2
+        assertEquals(6, helper.itemCount()); //should == 6
+        assertEquals(4, helper.pageItemCount(0)); //should == 4
+        assertEquals(2, helper.pageItemCount(1)); // last page - should == 2
+        assertEquals(-1, helper.pageItemCount(2)); // should == -1 since the page is invalid
+
+        // pageIndex takes an item index and returns the page that it belongs on
+        assertEquals(1, helper.pageIndex(5)); //should == 1 (zero based index)
+        assertEquals(0, helper.pageIndex(2)); //should == 0
+        assertEquals(-1, helper.pageIndex(20)); //should == -1
+        assertEquals(-1, helper.pageIndex(-10)); //should == -1
+
+    }
 }
